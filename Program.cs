f650e622eed37d994c767ce35b50aede6265006a
Sidney Crandall <sdncrandall5@gmail.com>
2021-05-21 11:41:34 -0500

    Intro into C#. Printed name in reverse

---
+++ Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+Console.Write("First Name> ");
+string firstName= Console.ReadLine();
+
+Console.Write("Last Name> ");
+string lastName = Console.ReadLine();
+
+//string name = firstName + " " + lastName;
+//Console.WriteLine(name);
+
+string reverse = string.Empty;
+string fullName = firstName + " " + lastName;
+
+for ( int i = fullName.Length - 1; i >= 0; i-- ) 
+{
+    reverse += fullName[i];
+}
+    Console.WriteLine(reverse);
+
+/*Console.Write("Who would you like to say hello to? ");
+
+string name = Console.ReadLine();
+
+if (string.IsNullOrWhiteSpace(name))
+{
+    Console.WriteLine("Fine, don't say 'hello'!");
+}
+else
+{
+    Console.WriteLine($"Hello, {name}!");
+}*/
